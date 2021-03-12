4357e0b82668bed1b16c6ec27b612abb1329dd87
Pranav13 <pranavshukla17@gmail.com>
2021-03-13 02:49:54 +0530

    Adding support for PDF

--- src/app/shopping/components/my-orders/my-orders.component.ts
+++ src/app/shopping/components/my-orders/my-orders.component.ts
@@ -2,9 +2,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { AuthenticationService } from 'shared/service/authentication.service';
 import { OrderService } from 'shared/service/order.service';
-//import jsPDF from 'jspdf';
-//import 'jspdf-autotable';
-//declare var jsPDF: any;
+import  jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-my-orders',
@@ -37,7 +37,51 @@ export class MyOrdersComponent implements OnInit,OnDestroy {
     //console.log(this.orders$);
   }
 
- 
+  generatePDF(order){
+    const doc = new jsPDF();
+    const columns = [['Product Name', 'Price', 'Quantity','Total Price','5']];
+    var data = [];
+
+    for(var i = 0 ;i<order.orderproduct.length;i++){
+       data.push(
+        [order.orderproduct[i].imageUrl, order.orderproduct[i].price, order.orderproduct[i].quantity,order.orderproduct[i].totalprice]
+       )
+    }
+   var total = '500';
+     autoTable(doc, {
+          head: columns,
+          body: data,
+          didDrawPage: (dataArg) => { 
+           doc.text('Your Order Total = ', dataArg.settings.margin.left, 10);
+           doc.text(total,65, 10);
+          }
+     }); 
+    
+     
+
+    doc.save('table.pdf');
+  }
+
+   bodyRows(rowCount) {
+    rowCount = rowCount || 10
+    var body = []
+    for (var j = 1; j <= rowCount; j++) {
+      body.push({
+        id: j,
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        city: faker.address.city(),
+        expenses: faker.finance.amount(),
+      })
+    }
+    return body
+  }
+
+   headRows() {
+    return [
+      { id: 'ID', name: 'Name', email: 'Email', city: 'City', expenses: 'Sum' },
+    ]
+  }
 
   ngOnDestroy(): void {
     this.dtTrigger.unsubscribe();
